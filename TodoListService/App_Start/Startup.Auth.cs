2c278a2092834ffd5de0f9a7bc8f8fa6b4421e2f
zhuoliseattle@gmail.com <seattle8482617>
2018-04-09 09:41:16 -0700

    First commit

---
+++ TodoListService/App_Start/Startup.Auth.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.ActiveDirectory;
+using Owin;
+
+namespace TodoListService
+{
+    public partial class Startup
+    {
+        // For more information on configuring authentication, please visit http://go.microsoft.com/fwlink/?LinkId=301864
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            // TODO: Configure Authentication for the Web API
+        }
+    }
+}
